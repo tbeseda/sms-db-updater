63af432ee1fcc4a0f75ae99955bbf0a71474ccec
Taylor Beseda <tbeseda@gmail.com>
2022-06-22 19:44:51 -0600

    env variable issue

--- src/http/post-sms/index.js
+++ src/http/post-sms/index.js
@@ -1,14 +1,14 @@
 const arc = require('@architect/functions');
 
-const { KNOWN_PHONES } = process.env;
-const ALLOWED = KNOWN_PHONES ? new Set(KNOWN_PHONES.split(',')) : new Set();
+const allowed = process.env.KNOWN_PHONES;
+// const ALLOWED = KNOWN_PHONES ? new Set(KNOWN_PHONES.split(',')) : new Set();
 
 async function http(request) {
   // https://developer.vonage.com/api/sms#webhooks
   const { body: message } = request;
   const { msisdn } = message;
 
-  if (ALLOWED.has(msisdn)) {
+  if (allowed && allowed.indexOf(msisdn) >= 0) {
     // background job to update banner message
     await arc.events.publish({
       name: 'new-sms',
