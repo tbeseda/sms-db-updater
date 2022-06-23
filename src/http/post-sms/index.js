94b778e25f43f3a628f6f8276568ccd729ad8380
Taylor Beseda <tbeseda@gmail.com>
2022-06-22 19:49:45 -0600

    Update index.js

--- src/http/post-sms/index.js
+++ src/http/post-sms/index.js
@@ -1,14 +1,12 @@
 const arc = require('@architect/functions');
-
 const allowed = process.env.KNOWN_PHONES;
-// const ALLOWED = KNOWN_PHONES ? new Set(KNOWN_PHONES.split(',')) : new Set();
 
 async function http(request) {
   // https://developer.vonage.com/api/sms#webhooks
-  const { body: message } = request;
-  const { msisdn } = message;
+  const message = request.body;
+  const from = message.msisdn;
 
-  if (allowed && allowed.indexOf(msisdn) >= 0) {
+  if (allowed && from && allowed.indexOf(from) >= 0) {
     // background job to update banner message
     await arc.events.publish({
       name: 'new-sms',
